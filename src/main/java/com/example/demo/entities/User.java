f0d1e9e52de7cea8e25c01b180bb92f770e15876
Jefferson Aguiar <jefferson.aguiar@facens.br>
2023-07-12 18:58:05 -0300

    feat: Add H2 database, test profile, JPA

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,17 +1,24 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User(int i, String maria, String mail) {
+    public User() {
+
     }
 
     public User(Long id, String name, String email, String phone, String password) {
