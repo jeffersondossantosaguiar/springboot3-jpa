d991354134fb6060466c9f9aefb1b903ab416db2
Jefferson Aguiar <jefferson.aguiar@facens.br>
2023-07-15 22:42:06 -0300

    feat: Order, instant, iso 8601

---
+++ src/main/java/com/example/demo/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Order;
+import com.example.demo.entities.User;
+import com.example.demo.repositories.OrderRepository;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> order = repository.findById(id);
+
+        return order.get();
+    }
+}
